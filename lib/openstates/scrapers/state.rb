237a4aa48fb8d063cd8a9d53625f7f34f7c60b39
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-04-04 06:48:09 -0500

    began configuring the structure of the state scrapers

---
+++ lib/openstates/scrapers/state.rb
@@ -0,0 +1,31 @@
+module State
+
+  class Metadata
+
+    # The two-letter abbreviation of the state
+    attr_reader :abbrevieation
+
+    # The name of the state
+    attr_reader :name
+  
+    attr_reader :legislature_name
+
+    attr_reader :lower_chamber_name
+
+    attr_reader :
+  
+  end
+
+  class Legislator
+  end
+
+  class Committee
+  end
+
+  class Event
+  end
+
+  class District
+  end
+
+end
